46d052e369d72ac57c75cb18350bfe9d5f1427d4
John Nolan <john.nolan@moneyplusgroup.co.uk>
2013-01-18 11:06:23 +0000

    initial commit

---
+++ OmerKata/CheckOutTests.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace OmerKata
+{
+
+    [TestFixture]
+    public class CheckOutTests
+    {
+        CheckOut _checkOut;
+
+        [SetUp]
+        public void SetUp()
+        {
+         var pricingRules = new Dictionary<string, decimal>();
+            pricingRules.Add("A",0.5m);
+            _checkOut = new CheckOut(pricingRules);
+        }
+
+        [Test]
+        public void EmptyBasketZero()
+        {           
+            Assert.That(_checkOut.Total, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void WhenScanningAnItemItReturnsCorrectPrice()
+        {            
+            _checkOut.Scan("A");
+            Assert.That(_checkOut.Total,Is.EqualTo(0.5m));
+        }
+    }
+
+    public class CheckOut
+    {
+        readonly Dictionary<string, decimal> _pricingRules;
+
+        public CheckOut(Dictionary<string, decimal> pricingRules)
+        {
+            _pricingRules = pricingRules;            
+        }
+
+        public decimal Total { get; private set; }
+
+        public void Scan(string item)
+        {
+            Total += _pricingRules[item];
+        }
+    }
+}
